b92dc5ad4f25a0d8d8e85e353d17a349af3806b6
Andrew Kane <andrew@ankane.org>
2021-03-15 01:26:01 -0700

    autocomplete -> autosuggest in tests [skip ci]

--- test/autosuggest_test.rb
+++ test/autosuggest_test.rb
@@ -3,86 +3,86 @@ require_relative "test_helper"
 class AutosuggestTest < Minitest::Test
   def test_similar_queries
     top_queries = {"chili" => 2, "chilli" => 1}
-    autocomplete = Autosuggest.new(top_queries)
-    assert autocomplete.suggestions.last[:duplicate]
+    autosuggest = Autosuggest.new(top_queries)
+    assert autosuggest.suggestions.last[:duplicate]
   end
 
   def test_stemming
     top_queries = {"tomato" => 2, "tomatoes" => 1}
-    autocomplete = Autosuggest.new(top_queries)
-    assert autocomplete.suggestions.last[:duplicate]
+    autosuggest = Autosuggest.new(top_queries)
+    assert autosuggest.suggestions.last[:duplicate]
   end
 
   def test_profanity
     top_queries = {"hell" => 2}
-    autocomplete = Autosuggest.new(top_queries)
-    assert autocomplete.suggestions.first[:profane]
+    autosuggest = Autosuggest.new(top_queries)
+    assert autosuggest.suggestions.first[:profane]
   end
 
   def test_duplicates
     top_queries = {"cage free eggs" => 2, "eggs cage free" => 1}
-    autocomplete = Autosuggest.new(top_queries)
-    assert autocomplete.suggestions.last[:duplicate]
+    autosuggest = Autosuggest.new(top_queries)
+    assert autosuggest.suggestions.last[:duplicate]
   end
 
   def test_not_duplicates
     top_queries = {"straws" => 2, "straus" => 1}
-    autocomplete = Autosuggest.new(top_queries)
-    autocomplete.not_duplicates([%w(straus straws)])
-    assert !autocomplete.suggestions.any? { |s| s[:duplicate] }
+    autosuggest = Autosuggest.new(top_queries)
+    autosuggest.not_duplicates([%w(straus straws)])
+    assert !autosuggest.suggestions.any? { |s| s[:duplicate] }
   end
 
   def test_block_words
     top_queries = {"test boom" => 2}
-    autocomplete = Autosuggest.new(top_queries)
-    autocomplete.block_words(["boom"])
-    assert autocomplete.suggestions.first[:blocked]
+    autosuggest = Autosuggest.new(top_queries)
+    autosuggest.block_words(["boom"])
+    assert autosuggest.suggestions.first[:blocked]
   end
 
   def test_block_words_phrase
     top_queries = {"test boom" => 2}
-    autocomplete = Autosuggest.new(top_queries)
-    autocomplete.block_words(["test boom"])
-    assert autocomplete.suggestions.first[:blocked]
+    autosuggest = Autosuggest.new(top_queries)
+    autosuggest.block_words(["test boom"])
+    assert autosuggest.suggestions.first[:blocked]
   end
 
   def test_blacklist
     top_queries = {"test boom" => 2}
-    autocomplete = Autosuggest.new(top_queries)
+    autosuggest = Autosuggest.new(top_queries)
     assert_output(nil, /deprecated/) do
-      autocomplete.blacklist_words(["boom"])
+      autosuggest.blacklist_words(["boom"])
     end
-    assert autocomplete.suggestions.first[:blacklisted]
+    assert autosuggest.suggestions.first[:blacklisted]
   end
 
   def test_blacklist_phrase
     top_queries = {"test boom" => 2}
-    autocomplete = Autosuggest.new(top_queries)
+    autosuggest = Autosuggest.new(top_queries)
     assert_output(nil, /deprecated/) do
-      autocomplete.blacklist_words(["test boom"])
+      autosuggest.blacklist_words(["test boom"])
     end
-    assert autocomplete.suggestions.first[:blacklisted]
+    assert autosuggest.suggestions.first[:blacklisted]
   end
 
   def test_prefer
     top_queries = {"amys" => 2}
-    autocomplete = Autosuggest.new(top_queries)
-    autocomplete.prefer(["amy's"])
-    assert_equal "amy's", autocomplete.suggestions.first[:query]
+    autosuggest = Autosuggest.new(top_queries)
+    autosuggest.prefer(["amy's"])
+    assert_equal "amy's", autosuggest.suggestions.first[:query]
   end
 
   def test_add_concept
     top_queries = {"amys" => 2}
-    autocomplete = Autosuggest.new(top_queries)
-    autocomplete.add_concept("brand", ["amys"])
-    assert_equal ["brand"], autocomplete.suggestions.first[:concepts]
+    autosuggest = Autosuggest.new(top_queries)
+    autosuggest.add_concept("brand", ["amys"])
+    assert_equal ["brand"], autosuggest.suggestions.first[:concepts]
   end
 
   def test_parse_words
     top_queries = {"tomato" => 2, "tomoto" => 1}
-    autocomplete = Autosuggest.new(top_queries)
-    autocomplete.parse_words ["tomato soup"]
-    refute autocomplete.suggestions.first[:misspelling]
-    assert autocomplete.suggestions.last[:misspelling]
+    autosuggest = Autosuggest.new(top_queries)
+    autosuggest.parse_words ["tomato soup"]
+    refute autosuggest.suggestions.first[:misspelling]
+    assert autosuggest.suggestions.last[:misspelling]
   end
 end
